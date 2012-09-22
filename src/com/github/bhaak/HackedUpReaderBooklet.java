1e455944de6e82eced6a5c0070505df2c44cca5c
Patric Mueller <bhaak@gmx.net>
2012-09-22 10:55:07 +0200

    Stop HackedUpReader when Booklet is stopped

--- src/com/github/bhaak/HackedUpReaderBooklet.java
+++ src/com/github/bhaak/HackedUpReaderBooklet.java
@@ -1,6 +1,7 @@
 package com.github.bhaak;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.net.URI;
 
 import com.amazon.kindle.booklet.AbstractBooklet;
@@ -19,48 +20,54 @@ public class HackedUpReaderBooklet extends AbstractBooklet {
 	private static final Log logger = Log.getInstance("HackedUpReaderBooklet");
 	private static final String hackedupreader = "/mnt/us/hackedupreader/bin/cr3";
 	
+	private Process hackedupreaderProcess; 
+	
 	public HackedUpReaderBooklet() {
 		logger.info("HackedUpReaderBooklet");
 	}
 	public void start(URI contentURI) {
 		logger.info("start("+contentURI+")");
 		try {
-			Runtime.getRuntime().exec(hackedupreader);
+			hackedupreaderProcess = Runtime.getRuntime().exec(hackedupreader);
 		} catch (IOException e) {
 			logger.error(e.toString(), e);
 		}
 		super.start(contentURI);
 	}
 
-	public void create(BookletContext bookletContext) {
-		logger.info("create("+bookletContext+")");
-		super.create(bookletContext);
-	}
-
-	public void destroy() {
-		logger.info("destroy()");
-		super.destroy();
-	}
-
-	public String getName() {
-		logger.info("getName()");
-		return super.getName();
-	}
 
 	public void stop() {
 		logger.info("stop()");
+		
+		if (hackedupreaderProcess != null) {
+			try {
+				killQuitProcess(hackedupreaderProcess);
+			} catch (Exception e) {
+				logger.error(e.toString(), e);
+			}
+		}
+		
 		super.stop();
 	}
-	public ChromeHeaderRequest getChromeHeaderRequest() {
-		logger.info("getChromeHeaderRequest()");
-		return super.getChromeHeaderRequest();
-	}
-	public void setChromeHeaderRequest(ChromeHeaderRequest chromeHeaderRequest) {
-		logger.info("setChromeHeaderRequest("+chromeHeaderRequest+")");
-		super.setChromeHeaderRequest(chromeHeaderRequest);
-	}
-	public void syncHeaderRequest() throws ChromeException {
-		logger.info("syncHeaderRequest()");
-		super.syncHeaderRequest();
+	
+	/** 
+	 * Send a QUIT signal to a process.
+	 * 
+	 * See http://stackoverflow.com/questions/2950338/how-can-i-kill-a-linux-process-in-java-with-sigkill-process-destroy-does-sigte#answer-2951193
+	 */
+	private void killQuitProcess(Process process)
+			throws InterruptedException, IOException, SecurityException, NoSuchFieldException, IllegalArgumentException, IllegalAccessException
+	{
+	    logger.info(process.getClass().getName());
+	    if (process.getClass().getName().equals("java.lang.UNIXProcess")) {
+	        Class cl = process.getClass();
+	        Field field = cl.getDeclaredField("pid");
+	        field.setAccessible(true);
+	        Object pidObject = field.get(process);
+	        
+	        Runtime.getRuntime().exec("kill -QUIT " + pidObject).waitFor();
+	    } else {
+	        throw new IllegalArgumentException("Needs to be a UNIXProcess");
+	    }
 	}
 }
