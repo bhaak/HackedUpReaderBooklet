6e6cd237734f71259628c8d3695e75862e0c531c
Patric Mueller <bhaak@gmx.net>
2013-01-30 22:37:34 +0100

    Add Kindlet based on Booklet

---
+++ src/com/github/bhaak/HackedUpReaderKindlet.java
@@ -0,0 +1,113 @@
+package com.github.bhaak;
+
+import com.amazon.kindle.kindlet.AbstractKindlet;
+import com.amazon.kindle.kindlet.KindletContext;
+
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+public class HackedUpReaderKindlet extends AbstractKindlet {
+
+	private static final Logger logger = Logger.getLogger(HackedUpReaderKindlet.class);
+	private final String hackedupreader = "/mnt/us/hackedupreader/bin/cr3";
+
+	private Process hackedupreaderProcess;
+	private HackedUpReaderWaitThread thread = null;
+
+	public void create(KindletContext context) {
+		Properties p = new Properties();
+		try {
+			p.load(this.getClass().getResource("log4j.properties").openStream());
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		PropertyConfigurator.configure(p);
+		
+		logger.info("create()");
+	}
+
+	public void start() {
+		try {
+			logger.info("Starting HackedUpReader");
+			
+			// Start HackedUpReader
+			hackedupreaderProcess = Runtime.getRuntime().exec(hackedupreader);
+			logger.info("Started HackedUpReader "+hackedupreaderProcess);
+			
+			thread = new HackedUpReaderWaitThread();
+			thread.start();
+		} catch (Throwable t) {
+			logger.error(t.getMessage(), t);
+		}
+	}
+
+
+	public void stop() {
+		logger.info("stop()");
+		// Stop HackedUpReader
+		if (hackedupreaderProcess != null) {
+			try {
+				thread.interrupt();
+				killQuitProcess(hackedupreaderProcess);
+			} catch (Exception e) {
+				logger.error(e.toString(), e);
+			}
+		}
+		super.stop();
+	}
+	
+	public void destroy() {
+		logger.info("destroy()");
+		super.destroy();
+	}
+
+	/** 
+	 * Send a QUIT signal to a process.
+	 * 
+	 * See http://stackoverflow.com/questions/2950338/how-can-i-kill-a-linux-process-in-java-with-sigkill-process-destroy-does-sigte#answer-2951193
+	 */
+	private void killQuitProcess(Process process)
+			throws InterruptedException, IOException, SecurityException, NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
+		if (process.getClass().getName().equals("java.lang.UNIXProcess")) {
+			Class cl = process.getClass();
+			Field field = cl.getDeclaredField("pid");
+			field.setAccessible(true);
+			Object pidObject = field.get(process);
+
+			Runtime.getRuntime().exec(new String[]{"/bin/kill", "-QUIT", pidObject.toString()}).waitFor();
+		} else {
+			throw new IllegalArgumentException("Needs to be a UNIXProcess");
+		}
+	}
+
+	/** This thread waits for HackedUpReader to finish and then sends
+	 * a BACKWARD lipc event. 
+	 */
+	class HackedUpReaderWaitThread extends Thread {
+		public void start() {
+			logger.info("HackedUpReaderWaitThread started");
+			super.start();
+		}
+		public void run() {
+			try {
+				logger.info("HackedUpReaderWaitThread run");
+
+				// wait for HackedUpReader to finish
+				hackedupreaderProcess.waitFor();
+				logger.info("HackedUpReader stopped");
+
+				// sent BACKWARD lipc event if HackedUpReader finished normally
+				Runtime.getRuntime().exec(new String[]{"/usr/bin/lipc-set-prop", "com.lab126.appmgrd", "backward", "0"});
+			} catch (IOException e) {
+				logger.error(e.toString(), e);
+			} catch (InterruptedException e) {
+				// Thread got interrupted, don't send lipc event
+				logger.warn(e.toString());
+			}
+		}
+	}
+}
